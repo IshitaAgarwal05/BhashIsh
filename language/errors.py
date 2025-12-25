239ff728d58f21b928f486f8fe7db5eb0988d706
Ishita Agarwal <111743570+IshitaAgarwal05@users.noreply.github.com>
2025-12-25 10:02:13 +0530

    fu removed

--- language/errors.py
+++ language/errors.py
@@ -1,6 +1,7 @@
 class HinglishError(Exception):
     def __init__(self, message, line=None, code_line=None, col=None):
-        output = "💀 fuck bhaii: " + message
+        # output = "💀 fuck bhaii: " + message
+        output = "💀 areee bhaii, error aagyaa: " + message
         if line and code_line and col is not None:
             output += f"\n    {code_line}\n    {' ' * col}^ yahin galti"
         super().__init__(output)
